8c0985ab81c6b37b67b528682665f2e28fdc4106
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2021-02-19 20:06:20 +0100

    Update new1.js

--- js/new1.js
+++ js/new1.js
@@ -1,1 +1,5 @@
-alert('test')
+var z = 'neuer Einsatz';
+document.getElementById("missionH1").innerHTML = "" + z + "";
+
+    $("#missionH1").click(function () {
+        var rename = prompt("Einsatz anlegen",""+z+"");
